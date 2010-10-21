da1925c9f03f52a90de6bc7154064224f3f05e74
Watson <watson1978@gmail.com>
2010-10-21 18:31:29 +0900

    cleanup

--- system_control.c
+++ system_control.c
@@ -5,15 +5,13 @@
 #import <AudioToolbox/AudioServices.h>
 #include <IOKit/pwr_mgt/IOPMLib.h>
 
-extern VALUE rb_sys_volume(VALUE obj, SEL sel);
-
 /*
  * call-seq: System::Power.sleep
  */
 void
 rb_sys_sleep(VALUE obj, SEL sel)
 {
-    mach_port_t port;
+    mach_port_t  port;
     io_connect_t manage;
 
     if (IOMasterPort(bootstrap_port, &port) != kIOReturnSuccess) {
@@ -37,9 +35,9 @@ AudioDeviceID get_audio_device_id(void)
     OSStatus      err;
     UInt32        size;
 
-    size = sizeof device;
+    size = sizeof(device);
     err = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice, &size, &device);
-    if(err != noErr) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to get device id");
     }
 
@@ -63,27 +61,27 @@ rb_sys_volume(VALUE obj, SEL sel)
     device = get_audio_device_id();
 
     // try set master volume (channel 0)
-    size = sizeof b_vol;
+    size = sizeof(b_vol);
     err = AudioDeviceGetProperty(device, 0, 0, kAudioDevicePropertyVolumeScalar, &size, &b_vol);
-    if(err == noErr) {
+    if (err == noErr) {
 	goto EXIT;
     }
-	
+
     // otherwise, try seperate channels
     // get channel numbers
     size = sizeof(channels);
     err = AudioDeviceGetProperty(device, 0, 0,kAudioDevicePropertyPreferredChannelsForStereo, &size, &channels);
-    if(err != noErr) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to get channel numbers");
     }
-    
+
     size = sizeof(float);
     err = AudioDeviceGetProperty(device, channels[0], 0, kAudioDevicePropertyVolumeScalar, &size, &volume[0]);
-    if(err != noErr) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to get volume of channel");
     }
     err = AudioDeviceGetProperty(device, channels[1], 0, kAudioDevicePropertyVolumeScalar, &size, &volume[1]);
-    if(err != noErr) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to get volume of channel");
     }
 
@@ -103,12 +101,12 @@ rb_sys_volume(VALUE obj, SEL sel)
 VALUE
 rb_sys_set_volume(VALUE obj, SEL sel, VALUE volume)
 {
-    AudioDeviceID      device;
-    OSStatus           err;
-    UInt32             size;
-    Boolean            canset = false;
-    UInt32             channels[2];
-    float              involume;
+    AudioDeviceID device;
+    OSStatus      err;
+    UInt32        size;
+    Boolean       canset = false;
+    UInt32        channels[2];
+    float         involume;
 
     if (!FIXFLOAT_P(volume)) {
 	rb_raise(rb_eArgError, "wrong type of argument");
@@ -124,7 +122,7 @@ rb_sys_set_volume(VALUE obj, SEL sel, VALUE volume)
 
     size = sizeof canset;
     err = AudioDeviceGetPropertyInfo(device, 0, false, kAudioDevicePropertyVolumeScalar, &size, &canset);
-    if(err == noErr && canset == true) {
+    if (err == noErr && canset == true) {
 	size = sizeof involume;
 	err = AudioDeviceSetProperty(device, NULL, 0, false, kAudioDevicePropertyVolumeScalar, size, &involume);
 	goto EXIT;
@@ -133,19 +131,19 @@ rb_sys_set_volume(VALUE obj, SEL sel, VALUE volume)
     // else, try seperate channes
     // get channels
     size = sizeof(channels);
-    err = AudioDeviceGetProperty(device, 0, false, kAudioDevicePropertyPreferredChannelsForStereo, &size,&channels);
-    if(err != noErr) {
+    err = AudioDeviceGetProperty(device, 0, false, kAudioDevicePropertyPreferredChannelsForStereo, &size, &channels);
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to get channel numbers");
     }
-        
+
     // set volume
     size = sizeof(float);
     err = AudioDeviceSetProperty(device, 0, channels[0], false, kAudioDevicePropertyVolumeScalar, size, &involume);
-    if(noErr != err) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to set volume of channel");
     }
     err = AudioDeviceSetProperty(device, 0, channels[1], false, kAudioDevicePropertyVolumeScalar, size, &involume);
-    if(noErr != err) {
+    if (err != noErr) {
 	rb_raise(rb_eRuntimeError, "Failed to set volume of channel");
     }
 
