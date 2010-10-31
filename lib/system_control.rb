c1af6be4c2376079d4b62bd77970defadf6f3979
Watson <watson1978@gmail.com>
2010-11-01 06:16:57 +0900

    add the System::Pboard

--- lib/system_control.rb
+++ lib/system_control.rb
@@ -1,2 +1,33 @@
 require 'system_control/system_control'
 
+module System
+  module Pboard
+    framework 'AppKit'
+
+    module_function
+
+    # call-seq: System::Pboard.paste
+    #
+    # This method get string from paste board.
+
+    def paste
+      pboard = NSPasteboard.generalPasteboard
+      pboard.stringForType(NSStringPboardType)
+    end
+
+    # call-seq: System::Pboard.copy(string) -> true or false
+    #
+    # This method set string to paste board.
+
+    def copy(string)
+      if (string.class != String)
+        string = string.to_s
+      end
+
+      pboard = NSPasteboard.generalPasteboard
+      pboard.declareTypes([NSStringPboardType], owner:nil);
+      pboard.setString(string, forType:NSStringPboardType)
+    end
+  end
+end
+
