2566f1d469ed01021a9ea765317b3f525f1df7a2
Watson <watson1978@gmail.com>
2011-02-03 13:48:21 +0900

    modified documentation.

--- lib/system_control.rb
+++ lib/system_control.rb
@@ -6,7 +6,8 @@ module System
 
     module_function
 
-    # call-seq: System::Pboard.paste -> string
+    # call-seq:
+    #   System::Pboard.paste -> string
     #
     # This method gets a string from the pasteboard.
     def paste
@@ -15,7 +16,8 @@ module System
       str ||= ""
     end
 
-    # call-seq: System::Pboard.read -> string
+    # call-seq:
+    #    System::Pboard.read -> string
     #
     # This method gets a string from the pasteboard.
     # - Alias of System::Pboard.paste
@@ -23,9 +25,10 @@ module System
       self.paste
     end
 
-    # call-seq: System::Pboard.copy(string) -> true or false
+    # call-seq:
+    #   System::Pboard.copy(string)
     #
-    # This method set a string to the pasteboard.
+    # This method sets a string to the pasteboard.
     def copy(string)
       if (string.class != String)
         string = string.to_s
@@ -36,15 +39,17 @@ module System
       pboard.setString(string, forType:NSStringPboardType)
     end
 
-    # call-seq: System::Pboard.write(string) -> true or false
+    # call-seq:
+    #   System::Pboard.write(string)
     #
-    # This method set a string to the pasteboard.
+    # This method sets a string to the pasteboard.
     # - Alias of System::Pboard.copy
     def write(string)
       self.copy(string)
     end
 
-    # call-seq: System::Pboard.clear
+    # call-seq:
+    #   System::Pboard.clear
     #
     # Clears the existing contents of the pasteboard.
     def clear
