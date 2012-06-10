b0883afec53d26d2d9b2dc68347ff82ec6ae932b
Watson <watson1978@gmail.com>
2012-06-10 22:07:28 +0900

    System::Sound module needs CoreAudio framework

--- ext/system_control/extconf.rb
+++ ext/system_control/extconf.rb
@@ -1,4 +1,4 @@
 require "mkmf"
 $CFLAGS << " -std=c99 "
-$LDFLAGS << " -framework ApplicationServices "
+$LDFLAGS << " -framework CoreAudio -framework ApplicationServices "
 create_makefile("system_control/system_control")
